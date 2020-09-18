2c61c118d8cb44f01a259780be6204a49fa1ccac
Arun <arunkumar1242k@gmail.com>
2020-09-18 14:25:48 +0530

    new function was added max_of_num

--- raj.py
+++ raj.py
@@ -1,6 +1,15 @@
 a= 5
 b=6
 def add(a,b):
-    return a+a
+    return a+b
 print(add(a,b))
 print("rajesj")
+
+def max_of_num(num1,num2,num3):
+    if num1>num2 and num1>num3:
+        return str(num1)+"is grater among all"
+    elif num2>num1 and num2>num3:
+        return str(num2)+"is grater among all"
+    else:
+        str(num3)+"is grater among all"
+print(max_of_num(5,8,1))
